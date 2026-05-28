554e00ae1675bce30b09dfaca4f644e9f324dc21
qalzhrany87-lab <qalzhrany87@gmail.com>
2026-05-28 13:24:26 -0700

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,14 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "university");
+$conn = new mysqli(
+"sql8.freesqldatabase.com",
+"sql8828600",
+"PASSWORD_HERE",
+"sql8828600"
+);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if($conn->connect_error){
+die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
